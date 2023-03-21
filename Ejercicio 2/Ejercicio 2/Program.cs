b977fbd8d9d28d59f5f930d482d1c7322ef22f3e
SerAngGom <al405280@uji.es>
2023-03-21 18:29:52 +0100

    Ejer 2 acabado

--- Ejercicio 2/Ejercicio 2/Program.cs
+++ Ejercicio 2/Ejercicio 2/Program.cs
@@ -10,6 +10,28 @@ namespace Ejercicio_2
     {
         static void Main(string[] args)
         {
+            double[] a = new double[] { 1, 2, 4, 5, 7 };
+            int cont = 0;
+            double suma = 0;
+            Program program= new Program();
+            Console.WriteLine(program.Media(a, cont, suma));
+            Console.ReadKey();
+        }
+
+        public double Media(double[] a, int cont, double suma)
+        {
+            if (cont < a.Length)
+            {
+                suma += a[cont];
+                cont += 1;
+                return Media(a, cont, suma);
+            }
+            else
+            {
+                return suma / a.Length;
+            }
+             
+            
         }
     }
 }
