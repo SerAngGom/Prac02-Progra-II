d71b9e4cf5f24bcad087e926f0f89fe9beacab01
PepeCanoRambla <jcanorambla@gmail.com>
2023-03-21 19:13:46 +0100

    Arreglado Ej3

--- Ejercicio 3/Ejercicio 3/Program.cs
+++ Ejercicio 3/Ejercicio 3/Program.cs
@@ -10,6 +10,35 @@ namespace Ejercicio_3
     {
         static void Main(string[] args)
         {
+            float[,] M = { { 1, 2, 3 }, { 4, 5, 6 } };
+            int n = 5;
+            int posx = 0;
+            int posy = 0;
+            Program program= new Program();
+            Console.WriteLine(program.ContenM( M, n, posx, posy));
+            Console.ReadKey();  
+
+        }
+
+        public bool ContenM(float[,] M, int n, int posx, int posy)
+        {
+            if (M[posx,posy] == n)
+            {
+                return true;
+            }
+            else if (posx < M.GetLength(0)-1)
+            {
+                posx++;
+                return ContenM( M, n, posx, posy);
+            }
+            else if(posy < M.GetLength(1)-1)
+            {
+                posx = 0;
+                posy++;
+                return ContenM( M, n, posx, posy);
+            }
+            return false;
+
         }
     }
 }
