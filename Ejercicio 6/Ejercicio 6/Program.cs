af7c553c32f9a00dbba2172452019d5ec6d47d84
SerAngGom <al405280@uji.es>
2023-04-12 16:50:44 +0200

    6 en proceso

--- Ejercicio 6/Ejercicio 6/Program.cs
+++ Ejercicio 6/Ejercicio 6/Program.cs
@@ -8,8 +8,52 @@ namespace Ejercicio_6
 {
     internal class Program
     {
+        int[,] matriz = { { 8, 0, 4, 4, 0, 2 }, { 4, 6, 0, 2, 0, 0 }, { 0, 0, 0, 8, 2, 0 } };
         static void Main(string[] args)
         {
+            int[,] matriz = { { 8, 0, 4, 4, 0, 2 }, { 4, 6, 0, 2, 0, 0 }, { 0, 0, 0, 8, 2, 0 } };
+            desplazarcerofil(matriz);
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write(matriz[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(matriz.GetLength(1));
+            Console.ReadKey();
+
+        }
+
+        
+        public static void desplazarcerofil(int[,] matriz)
+        {
+            int aux;
+            int auxfil;
+            int auxcol;
+            for (int fil = 0; fil < matriz.GetLength(0); fil++)
+            {
+                for (int col = 1; col < matriz.GetLength(1); col++)
+                {
+                    if (matriz[fil,col] == 0 && matriz[fil, col-1]!=0)
+                    {
+                        auxfil = fil;
+                        auxcol = col;
+                        while (matriz[fil, col - 1] != 0 ) ;
+                        {
+                            aux = matriz[fil, col - 1];
+                            matriz[fil, col] = aux;
+                            matriz[fil, col - 1] = 0;
+                            fil--;
+                            col--;
+                        }
+                        fil = auxfil;
+                        col = auxcol;
+                    }
+                }
+            }
         }
     }
+
 }
