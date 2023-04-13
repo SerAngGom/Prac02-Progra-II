638f5e3ff8fa27c48c453a5dfe07d80a25d8ef8c
SerAngGom <al405280@uji.es>
2023-04-13 23:57:50 +0200

    Ejercicio 10 diseño
    Colocacion de labels y buttons hecho

--- Ejercicio 10/Ejercicio 10/Form1.Designer.cs
+++ Ejercicio 10/Ejercicio 10/Form1.Designer.cs
@@ -28,7 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Casilla00 = new System.Windows.Forms.Label();
+            this.Casilla01 = new System.Windows.Forms.Label();
+            this.Casilla02 = new System.Windows.Forms.Label();
+            this.Casilla03 = new System.Windows.Forms.Label();
+            this.Casilla10 = new System.Windows.Forms.Label();
+            this.Casilla11 = new System.Windows.Forms.Label();
+            this.Casilla12 = new System.Windows.Forms.Label();
+            this.Casilla13 = new System.Windows.Forms.Label();
+            this.Casilla20 = new System.Windows.Forms.Label();
+            this.Casilla21 = new System.Windows.Forms.Label();
+            this.Casilla22 = new System.Windows.Forms.Label();
+            this.Casilla23 = new System.Windows.Forms.Label();
+            this.Casilla30 = new System.Windows.Forms.Label();
+            this.Casilla31 = new System.Windows.Forms.Label();
+            this.Casilla32 = new System.Windows.Forms.Label();
+            this.Casilla33 = new System.Windows.Forms.Label();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Casilla00
+            // 
+            this.Casilla00.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla00.Location = new System.Drawing.Point(244, 212);
+            this.Casilla00.Name = "Casilla00";
+            this.Casilla00.Size = new System.Drawing.Size(50, 50);
+            this.Casilla00.TabIndex = 0;
+            this.Casilla00.Text = "00";
+            this.Casilla00.Click += new System.EventHandler(this.Casilla00_Click);
+            // 
+            // Casilla01
+            // 
+            this.Casilla01.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla01.Location = new System.Drawing.Point(291, 212);
+            this.Casilla01.Name = "Casilla01";
+            this.Casilla01.Size = new System.Drawing.Size(50, 50);
+            this.Casilla01.TabIndex = 1;
+            this.Casilla01.Text = "01";
+            this.Casilla01.Click += new System.EventHandler(this.Casilla01_Click);
+            // 
+            // Casilla02
+            // 
+            this.Casilla02.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla02.Location = new System.Drawing.Point(337, 212);
+            this.Casilla02.Name = "Casilla02";
+            this.Casilla02.Size = new System.Drawing.Size(50, 50);
+            this.Casilla02.TabIndex = 2;
+            this.Casilla02.Text = "02";
+            this.Casilla02.Click += new System.EventHandler(this.Casilla02_Click);
+            // 
+            // Casilla03
+            // 
+            this.Casilla03.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla03.Location = new System.Drawing.Point(384, 212);
+            this.Casilla03.Name = "Casilla03";
+            this.Casilla03.Size = new System.Drawing.Size(50, 50);
+            this.Casilla03.TabIndex = 3;
+            this.Casilla03.Text = "03";
+            this.Casilla03.Click += new System.EventHandler(this.Casilla03_Click);
+            // 
+            // Casilla10
+            // 
+            this.Casilla10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla10.Location = new System.Drawing.Point(244, 262);
+            this.Casilla10.Name = "Casilla10";
+            this.Casilla10.Size = new System.Drawing.Size(50, 50);
+            this.Casilla10.TabIndex = 4;
+            this.Casilla10.Text = "01";
+            this.Casilla10.Click += new System.EventHandler(this.Casilla10_Click);
+            // 
+            // Casilla11
+            // 
+            this.Casilla11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla11.Location = new System.Drawing.Point(291, 262);
+            this.Casilla11.Name = "Casilla11";
+            this.Casilla11.Size = new System.Drawing.Size(50, 50);
+            this.Casilla11.TabIndex = 5;
+            this.Casilla11.Text = "11";
+            this.Casilla11.Click += new System.EventHandler(this.Casilla11_Click);
+            // 
+            // Casilla12
+            // 
+            this.Casilla12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla12.Location = new System.Drawing.Point(337, 262);
+            this.Casilla12.Name = "Casilla12";
+            this.Casilla12.Size = new System.Drawing.Size(50, 50);
+            this.Casilla12.TabIndex = 6;
+            this.Casilla12.Text = "12";
+            this.Casilla12.Click += new System.EventHandler(this.Casilla12_Click);
+            // 
+            // Casilla13
+            // 
+            this.Casilla13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla13.Location = new System.Drawing.Point(384, 262);
+            this.Casilla13.Name = "Casilla13";
+            this.Casilla13.Size = new System.Drawing.Size(50, 50);
+            this.Casilla13.TabIndex = 7;
+            this.Casilla13.Text = "13";
+            this.Casilla13.Click += new System.EventHandler(this.Casilla13_Click);
+            // 
+            // Casilla20
+            // 
+            this.Casilla20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla20.Location = new System.Drawing.Point(244, 312);
+            this.Casilla20.Name = "Casilla20";
+            this.Casilla20.Size = new System.Drawing.Size(50, 50);
+            this.Casilla20.TabIndex = 8;
+            this.Casilla20.Text = "20";
+            this.Casilla20.Click += new System.EventHandler(this.Casilla20_Click);
+            // 
+            // Casilla21
+            // 
+            this.Casilla21.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla21.Location = new System.Drawing.Point(291, 312);
+            this.Casilla21.Name = "Casilla21";
+            this.Casilla21.Size = new System.Drawing.Size(50, 50);
+            this.Casilla21.TabIndex = 9;
+            this.Casilla21.Text = "21";
+            this.Casilla21.Click += new System.EventHandler(this.Casilla21_Click);
+            // 
+            // Casilla22
+            // 
+            this.Casilla22.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla22.Location = new System.Drawing.Point(337, 312);
+            this.Casilla22.Name = "Casilla22";
+            this.Casilla22.Size = new System.Drawing.Size(50, 50);
+            this.Casilla22.TabIndex = 10;
+            this.Casilla22.Text = "22";
+            this.Casilla22.Click += new System.EventHandler(this.Casilla22_Click);
+            // 
+            // Casilla23
+            // 
+            this.Casilla23.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla23.Location = new System.Drawing.Point(384, 312);
+            this.Casilla23.Name = "Casilla23";
+            this.Casilla23.Size = new System.Drawing.Size(50, 50);
+            this.Casilla23.TabIndex = 11;
+            this.Casilla23.Text = "23";
+            this.Casilla23.Click += new System.EventHandler(this.Casilla23_Click);
+            // 
+            // Casilla30
+            // 
+            this.Casilla30.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla30.Location = new System.Drawing.Point(244, 362);
+            this.Casilla30.Name = "Casilla30";
+            this.Casilla30.Size = new System.Drawing.Size(50, 50);
+            this.Casilla30.TabIndex = 12;
+            this.Casilla30.Text = "30";
+            this.Casilla30.Click += new System.EventHandler(this.Casilla30_Click);
+            // 
+            // Casilla31
+            // 
+            this.Casilla31.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla31.Location = new System.Drawing.Point(291, 362);
+            this.Casilla31.Name = "Casilla31";
+            this.Casilla31.Size = new System.Drawing.Size(50, 50);
+            this.Casilla31.TabIndex = 13;
+            this.Casilla31.Text = "31";
+            this.Casilla31.Click += new System.EventHandler(this.Casilla31_Click);
+            // 
+            // Casilla32
+            // 
+            this.Casilla32.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla32.Location = new System.Drawing.Point(337, 362);
+            this.Casilla32.Name = "Casilla32";
+            this.Casilla32.Size = new System.Drawing.Size(50, 50);
+            this.Casilla32.TabIndex = 14;
+            this.Casilla32.Text = "32";
+            this.Casilla32.Click += new System.EventHandler(this.Casilla32_Click);
+            // 
+            // Casilla33
+            // 
+            this.Casilla33.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Casilla33.Location = new System.Drawing.Point(384, 362);
+            this.Casilla33.Name = "Casilla33";
+            this.Casilla33.Size = new System.Drawing.Size(50, 50);
+            this.Casilla33.TabIndex = 15;
+            this.Casilla33.Text = "33";
+            this.Casilla33.Click += new System.EventHandler(this.Casilla33_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(303, 138);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(75, 50);
+            this.buttonUp.TabIndex = 16;
+            this.buttonUp.Text = "Up";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Location = new System.Drawing.Point(303, 435);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(75, 50);
+            this.buttonDown.TabIndex = 17;
+            this.buttonDown.Text = "Down";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(142, 288);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(75, 50);
+            this.buttonLeft.TabIndex = 18;
+            this.buttonLeft.Text = "Left";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(462, 288);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(75, 50);
+            this.buttonRight.TabIndex = 19;
+            this.buttonRight.Text = "Right";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
             // 
             // Form1
             // 
@@ -36,6 +256,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(778, 744);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.Casilla33);
+            this.Controls.Add(this.Casilla32);
+            this.Controls.Add(this.Casilla31);
+            this.Controls.Add(this.Casilla30);
+            this.Controls.Add(this.Casilla23);
+            this.Controls.Add(this.Casilla22);
+            this.Controls.Add(this.Casilla21);
+            this.Controls.Add(this.Casilla20);
+            this.Controls.Add(this.Casilla13);
+            this.Controls.Add(this.Casilla12);
+            this.Controls.Add(this.Casilla11);
+            this.Controls.Add(this.Casilla10);
+            this.Controls.Add(this.Casilla03);
+            this.Controls.Add(this.Casilla02);
+            this.Controls.Add(this.Casilla01);
+            this.Controls.Add(this.Casilla00);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -45,6 +285,27 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Casilla00;
+        private System.Windows.Forms.Label Casilla01;
+        private System.Windows.Forms.Label Casilla02;
+        private System.Windows.Forms.Label Casilla03;
+        private System.Windows.Forms.Label Casilla10;
+        private System.Windows.Forms.Label Casilla11;
+        private System.Windows.Forms.Label Casilla12;
+        private System.Windows.Forms.Label Casilla13;
+        private System.Windows.Forms.Label Casilla20;
+        private System.Windows.Forms.Label Casilla21;
+        private System.Windows.Forms.Label Casilla22;
+        private System.Windows.Forms.Label Casilla23;
+        private System.Windows.Forms.Label Casilla30;
+        private System.Windows.Forms.Label Casilla31;
+        private System.Windows.Forms.Label Casilla32;
+        private System.Windows.Forms.Label Casilla33;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
     }
 }
 
--- Ejercicio 10/Ejercicio 10/Form1.cs
+++ Ejercicio 10/Ejercicio 10/Form1.cs
@@ -16,5 +16,105 @@ namespace Ejercicio_10
         {
             InitializeComponent();
         }
+
+        private void Casilla00_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla01_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla02_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla03_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla23_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla30_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla31_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla32_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Casilla33_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
