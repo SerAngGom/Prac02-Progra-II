313ce3c2768e8b1539db6b64cf93110af5943afb
SerAngGom <al405280@uji.es>
2023-04-14 17:33:33 +0200

    Ejer 10 cosas avanzadas

--- Ejercicio 10/Ejercicio 10/Form1.cs
+++ Ejercicio 10/Ejercicio 10/Form1.cs
@@ -15,7 +15,7 @@ namespace Ejercicio_10
      
     public partial class Form1 : Form
     {
-        int[,] matrizglobal = { { 2, 0, 0, 0 }, { 2, 0, 0, 0 }, { 2, 0, 0, 0 }, { 2, 0, 0, 0 } };
+        int[,] matrizglobal = { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 2, 2, 2, 2} };
         
         public Form1()
         {
@@ -26,104 +26,89 @@ namespace Ejercicio_10
 
         private void Casilla00_Click(object sender, EventArgs e)
         {
-            Casilla00.Text= Convert.ToString(matrizglobal[0, 0]);
         }
 
         private void Casilla01_Click(object sender, EventArgs e)
         {
-            Casilla01.Text = Convert.ToString(matrizglobal[0, 1]);
         }
 
         private void Casilla02_Click(object sender, EventArgs e)
         {
-            Casilla02.Text = Convert.ToString(matrizglobal[0, 2]);
         }
 
         private void Casilla03_Click(object sender, EventArgs e)
         {
-            Casilla03.Text = Convert.ToString(matrizglobal[0, 3]);
         }
 
         private void Casilla10_Click(object sender, EventArgs e)
         {
-            Casilla10.Text = Convert.ToString(matrizglobal[1, 0]);
         }
 
         private void Casilla11_Click(object sender, EventArgs e)
         {
-            Casilla11.Text = Convert.ToString(matrizglobal[1, 1]);
         }
 
         private void Casilla12_Click(object sender, EventArgs e)
         {
-            Casilla12.Text = Convert.ToString(matrizglobal[1, 2]);
         }
 
         private void Casilla13_Click(object sender, EventArgs e)
         {
-            Casilla13.Text = Convert.ToString(matrizglobal[1, 3]);
         }
 
         private void Casilla20_Click(object sender, EventArgs e)
         {
-            Casilla20.Text = Convert.ToString(matrizglobal[2, 0]);
         }
 
         private void Casilla21_Click(object sender, EventArgs e)
         {
-            Casilla21.Text = Convert.ToString(matrizglobal[2, 1]);
         }
 
         private void Casilla22_Click(object sender, EventArgs e)
         {
-            Casilla22.Text = Convert.ToString(matrizglobal[2, 2]);
         }
 
         private void Casilla23_Click(object sender, EventArgs e)
         {
-            Casilla23.Text = Convert.ToString(matrizglobal[2, 3]);
         }
 
         private void Casilla30_Click(object sender, EventArgs e)
         {
-            Casilla30.Text = Convert.ToString(matrizglobal[3, 0]);
         }
 
         private void Casilla31_Click(object sender, EventArgs e)
         {
-            Casilla31.Text = Convert.ToString(matrizglobal[3, 1]);
         }
 
         private void Casilla32_Click(object sender, EventArgs e)
         {
-            Casilla32.Text = Convert.ToString(matrizglobal[3, 2]);
         }
 
         private void Casilla33_Click(object sender, EventArgs e)
         {
-            Casilla33.Text = Convert.ToString(matrizglobal[3, 3]);
         }
 
         private void buttonUp_Click(object sender, EventArgs e)
         {
-
+            sumarUp(matrizglobal);
+            actualizarMatrizLabel(matrizglobal);
         }
 
         private void buttonDown_Click(object sender, EventArgs e)
         {
-            
             sumarDown(matrizglobal);
-            sumarDown(matrizglobal);
+            actualizarMatrizLabel(matrizglobal);
         }
 
         private void buttonLeft_Click(object sender, EventArgs e)
         {
-
+            actualizarMatrizLabel(matrizglobal);
         }
 
         private void buttonRight_Click(object sender, EventArgs e)
         {
-
+            sumarRight(matrizglobal);
+            actualizarMatrizLabel(matrizglobal);
         }
 
         public void Generadorde2(int[,] matrizglobal)
@@ -173,7 +158,106 @@ namespace Ejercicio_10
 
         public void ordenarUp(int[,] matrizglobal)
         {
+            int aux;
+            for (int col = 0; col < matrizglobal.GetLength(1); col++)
+            {
+                for (int fil = matrizglobal.GetLength(0)-2; fil >=0 ; fil--)
+                {
+                    if (matrizglobal[fil, col] == 0 && fil + 1 < matrizglobal.GetLength(0) && matrizglobal[fil + 1, col] != 0) //comprobar con traza pero funciona.
+                    {
+                        while (fil < matrizglobal.GetLength(0) - 1 && matrizglobal[fil + 1, col] != 0)
+                        {
+                            aux = matrizglobal[fil + 1, col];
+                            matrizglobal[fil, col] = aux;
+                            matrizglobal[fil + 1, col] = 0;
+                            fil++;
+                        }
+                    }
+                }
+            }
+        }
+        public void sumarUp(int[,] matrizglobal)
+        {
+            ordenarUp(matrizglobal);
+            int sum;
+            for (int col = 0; col < matrizglobal.GetLength(1); col++)
+            {
+                for (int fil = 1; fil < matrizglobal.GetLength(0); fil++)
+                {
+                    if (matrizglobal[fil, col] == matrizglobal[fil - 1, col])
+                    {
+                        sum = matrizglobal[fil, col] * 2;
+                        matrizglobal[fil - 1, col] = sum;
+                        matrizglobal[fil, col] = 0;
+                    }
+                }
+            }
+            ordenarUp(matrizglobal);
+        }
+        public void ordenarRight(int[,] matrizglobal) 
+        {
+            int aux;
+            for (int fil = 0; fil < matrizglobal.GetLength(0); fil++)
+            {
+                for (int col = 1; col < matrizglobal.GetLength(1); col++)
+                {
+                    if (matrizglobal[fil, col] == 0 && matrizglobal[fil, col - 1] != 0)
+                    {
+                        while (col != 0 && matrizglobal[fil, col - 1] != 0)
+                        {
+                            aux = matrizglobal[fil, col - 1];
+                            matrizglobal[fil, col] = aux;
+                            matrizglobal[fil, col - 1] = 0;
+                            col--;
+                        }
+                    }
+                }
+            }
+        }
+        public void sumarRight(int[,] matrizglobal) 
+        {
+            ordenarRight(matrizglobal);
+            int sum;
+            for (int fil = 0; fil < matrizglobal.GetLength(0); fil++)
+            {
+                for (int col = 0; col < matrizglobal.GetLength(1) - 1; col++)
+                {
+                    if (matrizglobal[fil, col] == matrizglobal[fil, col + 1])
+                    {
+                        sum = matrizglobal[fil, col] * 2;
+                        matrizglobal[fil, col + 1] = sum;
+                        matrizglobal[fil, col] = 0;
+                    }
+                }
+            }
+            ordenarRight(matrizglobal);
+        }
+        public void ordenarLeft(int[,] matrizglobal)
+        {
 
+        }
+        public void sumarLeft(int[,] matrizglobal)
+        {
+
+        }
+        public void actualizarMatrizLabel(int[,] matrizglobal)
+        {
+            Casilla00.Text = Convert.ToString(matrizglobal[0, 0]);
+            Casilla01.Text = Convert.ToString(matrizglobal[0, 1]);
+            Casilla02.Text = Convert.ToString(matrizglobal[0, 2]);
+            Casilla03.Text = Convert.ToString(matrizglobal[0, 3]);
+            Casilla10.Text = Convert.ToString(matrizglobal[1, 0]);
+            Casilla11.Text = Convert.ToString(matrizglobal[1, 1]);
+            Casilla12.Text = Convert.ToString(matrizglobal[1, 2]);
+            Casilla13.Text = Convert.ToString(matrizglobal[1, 3]);
+            Casilla20.Text = Convert.ToString(matrizglobal[2, 0]);
+            Casilla21.Text = Convert.ToString(matrizglobal[2, 1]);
+            Casilla22.Text = Convert.ToString(matrizglobal[2, 2]);
+            Casilla23.Text = Convert.ToString(matrizglobal[2, 3]);
+            Casilla30.Text = Convert.ToString(matrizglobal[3, 0]);
+            Casilla31.Text = Convert.ToString(matrizglobal[3, 1]);
+            Casilla32.Text = Convert.ToString(matrizglobal[3, 2]);
+            Casilla33.Text = Convert.ToString(matrizglobal[3, 3]);
         }
     }
 }
