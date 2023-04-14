5f48fb7381ead6fe466f1014ba3afbfddd12439b
SerAngGom <al405280@uji.es>
2023-04-14 18:44:09 +0200

    Ejercicio 10 avanzado

--- Ejercicio 10/Ejercicio 10/Form1.cs
+++ Ejercicio 10/Ejercicio 10/Form1.cs
@@ -12,16 +12,21 @@ using System.Windows.Forms;
 
 namespace Ejercicio_10
 {
-     
+
     public partial class Form1 : Form
     {
-        int[,] matrizglobal = { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 2, 2, 2, 2} };
-        
+        int[,] matrizglobal = { { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 }, { 2, 2, 2, 2 } };
+        Label[,] matrizlabel = { { Casilla00, Casilla01, Casilla02, Casilla03 }, { Casilla10, Casilla11, Casilla12, Casilla13 },
+            {Casilla20, Casilla21, Casilla22, Casilla23 }, {Casilla30,Casilla31, Casilla32, Casilla33 } };
+
         public Form1()
         {
             InitializeComponent();
+            Label[,] matrizlabel = { { Casilla00, Casilla01, Casilla02, Casilla03 }, { Casilla10, Casilla11, Casilla12, Casilla13 },
+            {Casilla20, Casilla21, Casilla22, Casilla23 }, {Casilla30,Casilla31, Casilla32, Casilla33 } };
 
         }
+
 
 
         private void Casilla00_Click(object sender, EventArgs e)
@@ -90,32 +95,34 @@ namespace Ejercicio_10
 
         private void buttonUp_Click(object sender, EventArgs e)
         {
+            ordenarUp(matrizglobal);
             sumarUp(matrizglobal);
-            actualizarMatrizLabel(matrizglobal);
+            generarNum2(matrizglobal);
+            actualizarMatrizLabelPrueba(matrizglobal, matrizlabel);
         }
 
         private void buttonDown_Click(object sender, EventArgs e)
         {
+            ordenarDown(matrizglobal);
             sumarDown(matrizglobal);
+            generarNum2(matrizglobal);
             actualizarMatrizLabel(matrizglobal);
         }
 
         private void buttonLeft_Click(object sender, EventArgs e)
         {
+            ordenarLeft(matrizglobal);
+            sumarLeft(matrizglobal);
+            generarNum2(matrizglobal);
             actualizarMatrizLabel(matrizglobal);
         }
 
         private void buttonRight_Click(object sender, EventArgs e)
         {
+            ordenarRight(matrizglobal);
             sumarRight(matrizglobal);
+            generarNum2(matrizglobal);
             actualizarMatrizLabel(matrizglobal);
-        }
-
-        public void Generadorde2(int[,] matrizglobal)
-        {
-            Random random = new Random();
-            int Posicion = random.Next(matrizglobal.Length);
-
         }
         public void ordenarDown(int[,] matrizglobal)
         {
@@ -139,7 +146,6 @@ namespace Ejercicio_10
         }
         public void sumarDown(int[,] matrizglobal)
         {
-            ordenarDown(matrizglobal);
             int sum;
             for (int col = 0; col < matrizglobal.GetLength(1); col++)
             {
@@ -161,7 +167,7 @@ namespace Ejercicio_10
             int aux;
             for (int col = 0; col < matrizglobal.GetLength(1); col++)
             {
-                for (int fil = matrizglobal.GetLength(0)-2; fil >=0 ; fil--)
+                for (int fil = matrizglobal.GetLength(0) - 2; fil >= 0; fil--)
                 {
                     if (matrizglobal[fil, col] == 0 && fil + 1 < matrizglobal.GetLength(0) && matrizglobal[fil + 1, col] != 0) //comprobar con traza pero funciona.
                     {
@@ -178,7 +184,6 @@ namespace Ejercicio_10
         }
         public void sumarUp(int[,] matrizglobal)
         {
-            ordenarUp(matrizglobal);
             int sum;
             for (int col = 0; col < matrizglobal.GetLength(1); col++)
             {
@@ -194,7 +199,7 @@ namespace Ejercicio_10
             }
             ordenarUp(matrizglobal);
         }
-        public void ordenarRight(int[,] matrizglobal) 
+        public void ordenarRight(int[,] matrizglobal)
         {
             int aux;
             for (int fil = 0; fil < matrizglobal.GetLength(0); fil++)
@@ -214,9 +219,8 @@ namespace Ejercicio_10
                 }
             }
         }
-        public void sumarRight(int[,] matrizglobal) 
+        public void sumarRight(int[,] matrizglobal)
         {
-            ordenarRight(matrizglobal);
             int sum;
             for (int fil = 0; fil < matrizglobal.GetLength(0); fil++)
             {
@@ -234,11 +238,40 @@ namespace Ejercicio_10
         }
         public void ordenarLeft(int[,] matrizglobal)
         {
-
+            int aux;
+            for (int fil = 0; fil < matrizglobal.GetLength(0); fil++)
+            {
+                for (int col = matrizglobal.GetLength(1) - 2; col >= 0; col--)
+                {
+                    if (matrizglobal[fil, col] == 0 && col + 1 < matrizglobal.GetLength(1) && matrizglobal[fil, col + 1] != 0)
+                    {
+                        while (col < matrizglobal.GetLength(1) - 1 && matrizglobal[fil, col + 1] != 0)
+                        {
+                            aux = matrizglobal[fil, col + 1];
+                            matrizglobal[fil, col] = aux;
+                            matrizglobal[fil, col + 1] = 0;
+                            col++;
+                        }
+                    }
+                }
+            }
         }
         public void sumarLeft(int[,] matrizglobal)
         {
-
+            int sum;
+            for (int fil = 0; fil < matrizglobal.GetLength(0); fil++)
+            {
+                for (int col = 1; col < matrizglobal.GetLength(1); col++)
+                {
+                    if (matrizglobal[fil, col] == matrizglobal[fil, col - 1])
+                    {
+                        sum = matrizglobal[fil, col] * 2;
+                        matrizglobal[fil, col - 1] = sum;
+                        matrizglobal[fil, col] = 0;
+                    }
+                }
+            }
+            ordenarLeft(matrizglobal);
         }
         public void actualizarMatrizLabel(int[,] matrizglobal)
         {
@@ -258,6 +291,64 @@ namespace Ejercicio_10
             Casilla31.Text = Convert.ToString(matrizglobal[3, 1]);
             Casilla32.Text = Convert.ToString(matrizglobal[3, 2]);
             Casilla33.Text = Convert.ToString(matrizglobal[3, 3]);
+        }
+
+        public void actualizarMatrizLabelPrueba(int[,] matrizglobal, Label[,] matrizlabel)
+        {
+            for (int fil = 0; fil < matrizglobal.GetLength(0); fil++)
+            {
+                for (int col = 0; col < matrizglobal.GetLength(1); col++)
+                {
+                    matrizlabel[fil, col].Text= Convert.ToString(matrizglobal[fil, col]);
+                }
+            }
+        }
+
+        public void generarNum2(int[,] matrizglobal)
+        {
+            int[] arrayceros = CrearArrayCeros(matrizglobal);
+            Random random = new Random();
+            int posicionAleatoria = random.Next(arrayceros.Length);
+            int fila;
+            int columna;
+            fila = arrayceros[posicionAleatoria] / matrizglobal.GetLength(1);
+            columna = arrayceros[posicionAleatoria] % matrizglobal.GetLength(1);
+            matrizglobal[fila, columna] = 2;
+        }
+
+        public int[] CrearArrayCeros(int[,] matriz)
+        {
+            int contador = 0;
+
+            for (int fil = 0; fil < matriz.GetLength(0); fil++)
+            {
+                for (int col = 0; col < matriz.GetLength(1); col++)
+                {
+                    if (matriz[fil, col] == 0)
+                    {
+                        contador++;
+                    }
+                }
+
+            }
+            int[] array = new int[contador];
+            contador = 0;
+            int posicion = 0;
+            int fila;
+            int columna;
+            while (contador < array.Length)
+            {
+                fila = posicion / matriz.GetLength(1);
+                columna = posicion % matriz.GetLength(1);
+                if (matriz[fila, columna] == 0)
+                {
+                    array[contador] = posicion;
+                    contador++;
+                }
+                posicion++;
+            }
+            return array;
+
         }
     }
 }
